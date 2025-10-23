f44a4bfc86701c2a179d6c8caf36660040ac3ee9
tnthieu <trunghieu9102007@gmail.com>
2025-10-23 16:51:51 +0700

    push the code to github

---
+++ 2.4/src/Invoice.java
@@ -0,0 +1,60 @@
+public class Invoice {
+    private int id;
+    private Customer customer;
+    private double amount;
+
+    public Invoice(int id, Customer customer, double amount) {
+        this.id = id;
+        this.customer = customer;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getCustomerId() {
+        return customer.getId();
+    }
+
+    public String getCustomerName() {
+        return customer.getName();
+    }
+
+    public int getCustomerDiscount() {
+        return customer.getDiscount();
+    }
+
+    public double getAmountAfterDiscount() {
+        return amount;
+    }
+
+    public void setAmountAfterDiscount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "amount=" + amount +
+                ", id=" + id +
+                ", customer=" + customer +
+                '}';
+    }
+}
